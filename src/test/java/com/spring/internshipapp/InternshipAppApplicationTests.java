df601c80e60f3cc659d700c3064cc8717c094488
Aleksandar Siljanoski <acewow3@gmail.com>
2024-04-04 15:46:56 +0200

    Initial commit

---
+++ src/test/java/com/spring/internshipapp/InternshipAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.internshipapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
